KDE/kdepimlibs

Used cached version of thatIsMe

kcalutils/incidenceformatter.cpp
@@ -45,7 +45,7 @@
 #include <kcalcore/visitor.h>
 using namespace KCalCore;
 
-#include <kpimidentities/identitymanager.h>
+#include <kpimidentities/utils.h>
 
 #include <kpimutils/email.h>
 #include <kpimutils/linklocator.h>
@@ -78,42 +78,15 @@ static QString string2HTML(const QString &str)
     return KPIMUtils::LinkLocator::convertToHtml(str);
 }
 
-static KPIMIdentities::IdentityManager *s_identityManager = 0;
-
-// Performance optimization so we only create one IdentityManager instead of 1 per attendee.
-// Using RAII to protect against future return statements in the middle of code
-struct RAIIIdentityManager{
-    RAIIIdentityManager()
-    {
-        //t.start();
-        s_identityManager = new KPIMIdentities::IdentityManager(true);
-    }
-
-    ~RAIIIdentityManager()
-    {
-        delete s_identityManager;
-        s_identityManager = 0;
-        //qDebug() << "Elapsed time: " << t.elapsed();
-    }
-    //QElapsedTimer t;
-};
-
 static bool thatIsMe(const QString &email)
 {
-    return s_identityManager ? s_identityManager->thatIsMe(email)
-                             : KPIMIdentities::IdentityManager(true).thatIsMe(email);
+    return KPIMIdentities::thatIsMe(email);
 }
 
-static bool iamAttendee(Attendee::Ptr attendee)
+static bool iamAttendee(const Attendee::Ptr &attendee)
 {
     // Check if this attendee is the user
     return thatIsMe(attendee->email());
-}
-
-static bool iamPerson(const Person &person)
-{
-    // Check if this person is the user. test email only
-    return thatIsMe(person.email());
 }
 
 static QString htmlAddLink(const QString &ref, const QString &text,
@@ -130,22 +103,20 @@ static QString htmlAddMailtoLink(const QString &email, const QString &name)
 {
     QString str;
 
-    if (!email.isEmpty()) {
+    if (!email.isEmpty() && !thatIsMe(email)) {
         Person person(name, email);
-        if (!iamPerson(person)) {     // do not add a link for the user's email
-            QString path = person.fullName().simplified();
-            if (path.isEmpty() || path.startsWith(QLatin1Char('"'))) {
-                path = email;
-            }
-            KUrl mailto;
-            mailto.setProtocol(QLatin1String("mailto"));
-            mailto.setPath(path);
-
-            // static for performance
-            static const QString iconPath =
-                KIconLoader::global()->iconPath(QLatin1String("mail-message-new"), KIconLoader::Small);
-            str = htmlAddLink(mailto.url(), QLatin1String("<img valign=\"top\" src=\"") + iconPath + QLatin1String("\">"));
+        QString path = person.fullName().simplified();
+        if (path.isEmpty() || path.startsWith(QLatin1Char('"'))) {
+            path = email;
         }
+        KUrl mailto;
+        mailto.setProtocol(QLatin1String("mailto"));
+        mailto.setPath(path);
+
+        // static for performance
+        static const QString iconPath =
+            KIconLoader::global()->iconPath(QLatin1String("mail-message-new"), KIconLoader::Small);
+        str = htmlAddLink(mailto.url(), QLatin1String("<img valign=\"top\" src=\"") + iconPath + QLatin1String("\">"));
     }
     return str;
 }
@@ -1318,7 +1289,6 @@ static Attendee::Ptr findDelegatedFromMyAttendee(const Incidence::Ptr &incidence
         return attendee;
     }
 
-    RAIIIdentityManager raiiHelper;
     QString delegatorName, delegatorEmail;
     Attendee::List attendees = incidence->attendees();
     Attendee::List::ConstIterator it;
@@ -1343,12 +1313,11 @@ static Attendee::Ptr findMyAttendee(const Incidence::Ptr &incidence)
         return attendee;
     }
 
-    RAIIIdentityManager raiiHelper;
     Attendee::List attendees = incidence->attendees();
     Attendee::List::ConstIterator it;
     for (it = attendees.constBegin(); it != attendees.constEnd(); ++it) {
         Attendee::Ptr a = *it;
-        if (thatIsMe(a->email())) {
+        if (iamAttendee(a)) {
             attendee = a;
             break;
         }
@@ -1367,7 +1336,6 @@ static Attendee::Ptr findAttendee(const Incidence::Ptr &incidence,
         return attendee;
     }
 
-    RAIIIdentityManager raiiHelper;
     Attendee::List attendees = incidence->attendees();
     Attendee::List::ConstIterator it;
     for (it = attendees.constBegin(); it != attendees.constEnd(); ++it) {
@@ -2408,8 +2376,6 @@ static QString invitationHeaderFreeBusy(const FreeBusy::Ptr &fb,
 
 static QString invitationAttendeeList(const Incidence::Ptr &incidence)
 {
-    RAIIIdentityManager raiiHelper;
-
     QString tmpStr;
     if (!incidence) {
         return tmpStr;

KDE/kcalutils

Conflicts:
	src/incidenceformatter.cpp

src/incidenceformatter.cpp
@@ -46,6 +46,7 @@
 using namespace KCalCore;
 
 #include <identitymanager.h>
+#include <utils.h>
 
 #include <kemailaddress.h>
 #include <ktexttohtml.h>
@@ -80,42 +81,15 @@ static QString string2HTML(const QString &str)
     return KTextToHTML::convertToHtml(str, KTextToHTML::HighlightText | KTextToHTML::ReplaceSmileys);
 }
 
-static KIdentityManagement::IdentityManager *s_identityManager = Q_NULLPTR;
-
-// Performance optimization so we only create one IdentityManager instead of 1 per attendee.
-// Using RAII to protect against future return statements in the middle of code
-struct RAIIIdentityManager {
-    RAIIIdentityManager()
-    {
-        //t.start();
-        s_identityManager = new KIdentityManagement::IdentityManager(true);
-    }
-
-    ~RAIIIdentityManager()
-    {
-        delete s_identityManager;
-        s_identityManager = Q_NULLPTR;
-        //qCDebug(KCALUTILS_LOG) << "Elapsed time: " << t.elapsed();
-    }
-    //QElapsedTimer t;
-};
-
 static bool thatIsMe(const QString &email)
 {
-    return s_identityManager ? s_identityManager->thatIsMe(email)
-           : KIdentityManagement::IdentityManager(true).thatIsMe(email);
+    return KIdentityManagement::thatIsMe(email);
 }
 
-static bool iamAttendee(Attendee::Ptr attendee)
+static bool iamAttendee(const Attendee::Ptr &attendee)
 {
     // Check if this attendee is the user
     return thatIsMe(attendee->email());
-}
-
-static bool iamPerson(const Person &person)
-{
-    // Check if this person is the user. test email only
-    return thatIsMe(person.email());
 }
 
 static QString htmlAddLink(const QString &ref, const QString &text,
@@ -132,22 +106,20 @@ static QString htmlAddMailtoLink(const QString &email, const QString &name)
 {
     QString str;
 
-    if (!email.isEmpty()) {
+    if (!email.isEmpty() && !thatIsMe(email)) {
         Person person(name, email);
-        if (!iamPerson(person)) {     // do not add a link for the user's email
-            QString path = person.fullName().simplified();
-            if (path.isEmpty() || path.startsWith(QLatin1Char('"'))) {
-                path = email;
-            }
+        QString path = person.fullName().simplified();
+        if (path.isEmpty() || path.startsWith(QLatin1Char('"'))) {
+            path = email;
+        }
             QUrl mailto;
             mailto.setScheme(QLatin1String("mailto"));
-            mailto.setPath(path);
+        mailto.setPath(path);
 
-            // static for performance
-            static const QString iconPath =
-                KIconLoader::global()->iconPath(QLatin1String("mail-message-new"), KIconLoader::Small);
-            str = htmlAddLink(mailto.url(), QLatin1String("<img valign=\"top\" src=\"") + iconPath + QLatin1String("\">"));
-        }
+        // static for performance
+        static const QString iconPath =
+            KIconLoader::global()->iconPath(QLatin1String("mail-message-new"), KIconLoader::Small);
+        str = htmlAddLink(mailto.url(), QLatin1String("<img valign=\"top\" src=\"") + iconPath + QLatin1String("\">"));
     }
     return str;
 }
@@ -1320,7 +1292,6 @@ static Attendee::Ptr findDelegatedFromMyAttendee(const Incidence::Ptr &incidence
         return attendee;
     }
 
-    RAIIIdentityManager raiiHelper;
     QString delegatorName, delegatorEmail;
     Attendee::List attendees = incidence->attendees();
     Attendee::List::ConstIterator it;
@@ -1345,12 +1316,11 @@ static Attendee::Ptr findMyAttendee(const Incidence::Ptr &incidence)
         return attendee;
     }
 
-    RAIIIdentityManager raiiHelper;
     Attendee::List attendees = incidence->attendees();
     Attendee::List::ConstIterator it;
     for (it = attendees.constBegin(); it != attendees.constEnd(); ++it) {
         Attendee::Ptr a = *it;
-        if (thatIsMe(a->email())) {
+        if (iamAttendee(a)) {
             attendee = a;
             break;
         }
@@ -1369,7 +1339,6 @@ static Attendee::Ptr findAttendee(const Incidence::Ptr &incidence,
         return attendee;
     }
 
-    RAIIIdentityManager raiiHelper;
     Attendee::List attendees = incidence->attendees();
     Attendee::List::ConstIterator it;
     for (it = attendees.constBegin(); it != attendees.constEnd(); ++it) {
@@ -2403,8 +2372,6 @@ static QString invitationHeaderFreeBusy(const FreeBusy::Ptr &fb,
 
 static QString invitationAttendeeList(const Incidence::Ptr &incidence)
 {
-    RAIIIdentityManager raiiHelper;
-
     QString tmpStr;
     if (!incidence) {
         return tmpStr;