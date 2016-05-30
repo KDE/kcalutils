3a71fcbea8394e77eee4d66b49473293c179095d
Albert Astals Cid <aacid@kde.org>
2016-05-31 00:39:25 +0200

    Use $EXTRACT_GRANTLEE_TEMPLATE_STRINGS
    As documented in https://techbase.kde.org/Development/Tutorials/Localization/i18n_Build_Systems#Writing_a_Messages.sh_script
    CCMAIL: dvratil@kde.org

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-scripts/extract_strings_ki18n.py `find templates -name \*.html` >> html.cpp
+$EXTRACT_GRANTLEE_TEMPLATE_STRINGS `find templates -name \*.html` >> html.cpp
 $EXTRACTRC *.kcfg *.ui >> rc.cpp
 $XGETTEXT rc.cpp html.cpp src/*.cpp src/*.h -o $podir/libkcalutils5.pot
 rm -f rc.cpp html.cpp
