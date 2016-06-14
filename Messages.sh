32b09ea7b1d5606f0ebfb9588b6d0b5014240401
Burkhard Lück <lueck@hube-lueck.de>
2016-06-14 10:40:47 +0200

    fix Messages.sh, templates dir was moved

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-$EXTRACT_GRANTLEE_TEMPLATE_STRINGS `find templates -name \*.html` >> html.cpp
+$EXTRACT_GRANTLEE_TEMPLATE_STRINGS `find src/templates -name \*.html` >> html.cpp
 $EXTRACTRC *.kcfg *.ui >> rc.cpp
 $XGETTEXT rc.cpp html.cpp src/*.cpp src/*.h -o $podir/libkcalutils5.pot
 rm -f rc.cpp html.cpp
