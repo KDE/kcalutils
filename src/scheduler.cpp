d07064585390dc8cb8158b95c1022fc59676ed63
Montel Laurent <montel@kde.org>
2017-07-06 20:21:25 +0200

    Minor optimization

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -546,8 +546,7 @@ bool Scheduler::acceptFreeBusy(const IncidenceBase::Ptr &incidence, iTIPMethod m
     Person::Ptr from;
     if (method == iTIPPublish) {
         from = freebusy->organizer();
-    }
-    if ((method == iTIPReply) && (freebusy->attendeeCount() == 1)) {
+    } else if ((method == iTIPReply) && (freebusy->attendeeCount() == 1)) {
         Attendee::Ptr attendee = freebusy->attendees().at(0);
         from->setName(attendee->name());
         from->setEmail(attendee->email());
