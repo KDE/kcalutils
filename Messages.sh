4bbc9e5cde9048e1473a9d69fda8253203b0764c
Laurent Montel <montel@kde.org>
2021-12-09 13:23:40 +0100

    GIT_SILENT: fix some reuse lint warning

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: none
 $EXTRACT_GRANTLEE_TEMPLATE_STRINGS `find src/templates -name \*.html` >> rc.cpp
 $EXTRACTRC *.kcfg *.ui >> rc.cpp
 $XGETTEXT rc.cpp src/*.cpp src/*.h -o $podir/libkcalutils5.pot
