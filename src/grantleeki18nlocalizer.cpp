ce42de34d5cf3a7e50a490c89644d79ebfaf3356
Laurent Montel <montel@kde.org>
2023-03-28 20:29:45 +0200

    Fix show text in qt6

--- src/grantleeki18nlocalizer.cpp
+++ src/grantleeki18nlocalizer.cpp
@@ -92,15 +92,14 @@ QString GrantleeKi18nLocalizer::processArguments(const KLocalizedString &kstr, c
             str = str.subs(iter->toDouble());
             break;
         case QMetaType::User:
+        default:
             if (iter->canConvert<KTextTemplate::SafeString>()) {
                 str = str.subs(iter->value<KTextTemplate::SafeString>().get());
                 break;
+            } else {
+                qCWarning(KCALUTILS_LOG) << "Unknown type" << iter->typeName() << "(" << iter->userType() << ")";
+                break;
             }
-            // fall-through
-            Q_FALLTHROUGH();
-        default:
-            qCWarning(KCALUTILS_LOG) << "Unknown type" << iter->typeName() << "(" << iter->userType() << ")";
-            break;
         }
 #endif
     }
