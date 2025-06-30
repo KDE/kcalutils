aa20bf38a65ef9d95391aa5ea615152e2de455e4
Laurent Montel <montel@kde.org>
2025-06-30 20:52:20 +0200

    Add .gersemirc

--- cmake/KPIMGrantleeMacros.cmake
+++ cmake/KPIMGrantleeMacros.cmake
@@ -4,12 +4,14 @@ macro(kpim_grantlee_adjust_plugin_name pluginname)
     set_target_properties(
         ${pluginname}
         PROPERTIES
-            PREFIX ""
+            PREFIX
+                ""
             LIBRARY_OUTPUT_DIRECTORY
                 "${CMAKE_BINARY_DIR}/bin/grantlee/${Grantlee5_VERSION_MAJOR}.${Grantlee5_VERSION_MINOR}"
             RUNTIME_OUTPUT_DIRECTORY
                 "${CMAKE_BINARY_DIR}/bin/grantlee/${Grantlee5_VERSION_MAJOR}.${Grantlee5_VERSION_MINOR}"
-            DEBUG_POSTFIX "d"
+            DEBUG_POSTFIX
+                "d"
     )
     foreach(cfg ${CMAKE_CONFIGURATION_TYPES})
         string(TOUPPER ${cfg} CFG)
