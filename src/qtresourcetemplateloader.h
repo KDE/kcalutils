9c13c8ce05c4e75c9264e355c7948a261c98ea56
Laurent Montel <montel@kde.org>
2025-12-29 06:48:54 +0100

    Add missing explicit

--- src/qtresourcetemplateloader.h
+++ src/qtresourcetemplateloader.h
@@ -14,7 +14,7 @@ namespace KCalUtils
 class QtResourceTemplateLoader : public KTextTemplate::FileSystemTemplateLoader
 {
 public:
-    QtResourceTemplateLoader(const QSharedPointer<KTextTemplate::AbstractLocalizer> &localizer = QSharedPointer<KTextTemplate::AbstractLocalizer>());
+    explicit QtResourceTemplateLoader(const QSharedPointer<KTextTemplate::AbstractLocalizer> &localizer = QSharedPointer<KTextTemplate::AbstractLocalizer>());
 
     [[nodiscard]] KTextTemplate::Template loadByName(const QString &fileName, const KTextTemplate::Engine *engine) const override;
     [[nodiscard]] bool canLoadTemplate(const QString &name) const override;
